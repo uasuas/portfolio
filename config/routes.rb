8731ab9bb3b6ffe1c935f9445567c8ee91b21f5a
usausa <143808163+uasuas@users.noreply.github.com>
2023-12-04 05:30:01 +0000

    [add] public/admin/first_transition

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,34 @@
 Rails.application.routes.draw do
-devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  root 'public/homes#top'
+  get 'about', to: 'public/homes#about'
+
+  scope module: :public do
+    resources :companies, only: %i[index show]
+    resources :line_plans, only: %i[index show]
+    resource :customers do
+      collection do
+        get :mypage, to: "customers#show"
+        get "mypage/edit", to: "customers#edit"
+        patch "mypage/update", to: "customers#update"
+        get :confirm
+        patch :leave
+      end
+    end
+    # get 'searches/search', as: "customer_search"
+  end
+
+  namespace :admin do
+    root "inquiries#index"
+    get "inquiries/show", to: "inquiries#show"
+  end
+
 end
