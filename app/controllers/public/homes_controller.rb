9a8ee46b5afaffac0c0fb98d5dad1691578366e6
usausa <143808163+uasuas@users.noreply.github.com>
2023-12-05 04:36:30 +0000

    [update] seed作成

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,38 @@
 class Public::HomesController < ApplicationController
   def top
+    @areas = {
+      "東京" => "１",
+      "千葉" => "２",
+      "埼玉" => "３",
+      "神奈川" => "４",
+      "群馬" => "５",
+      "栃木" => "６",
+      "茨城" => "７",
+      "山梨" => "８"
+    }
+    @contents = {
+      "テレビ" => "１",
+      "ネット" => "２",
+      "電話" => "４",
+      "テレビ＋ネット" => "３",
+      "テレビ＋電話" => "５",
+      "ネット＋電話" => "６",
+      "テレビ＋ネット＋電話" => "７"
+    }
+    @prices = {
+      "１０００〜２０００" => "１",
+      "２００１〜３０００" => "２",
+      "３００１〜４０００" => "３",
+      "４００１〜５０００" => "４",
+      "５００１〜６０００" => "５"
+    }
   end
 
   def about
+    areas = params[:area]
+    contents = params[:contents]
+    prices = params[:price]
+    @plan = (areas.to_s + contents.to_s + prices.to_s).presence
+    @plan_details = PlanDetail.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,49 @@ Admin.create(
   email: "admin@admin",
   password: "adminadmin",
 )
+
+# 診断結果用seed
+areas = {
+  "東京" => "１",
+  "千葉" => "２",
+  "埼玉" => "３",
+  "神奈川" => "４",
+  "群馬" => "５",
+  "栃木" => "６",
+  "茨城" => "７",
+  "山梨" => "８"
+}
+
+# 内容
+contents = {
+  "テレビ回線" => "１",
+  "インターネット回線" => "２",
+  "電話回線" => "４",
+  "テレビ回線・インターネット回線" => "３",
+  "テレビ回線・電話回線回線" => "５",
+  "インターネット回線・電話回線" => "６",
+  "テレビ回線・インターネット回線・電話回線" => "７"
+}
+
+# 料金
+prices = {
+  "1,000円〜2,000円" => "１",
+  "2,001円〜3,000円" => "２",
+  "3,001円〜4,000円" => "３",
+  "4,001円〜5,000円" => "４",
+  "5,001円〜6,000円" => "５",
+  "6,000円~" => "6",
+}
+
+# seedデータの生成
+areas.each do |area_name, area_code|
+  contents.each do |content_name, content_code|
+    prices.each do |price_range, price_code|
+      # シードデータの作成
+      PlanDetail.create(
+        plan_code: "#{area_code}#{content_code}#{price_code}",
+        plan_text: "こちらは#{area_name}/#{content_name}/料金プランは#{price_range}のご契約プランです。",
+      )
+    end
+  end
+end
