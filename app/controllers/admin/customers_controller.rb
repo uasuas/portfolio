0e2c733a9063746897a57467635035492ced2f05
usausa <143808163+uasuas@users.noreply.github.com>
2023-12-11 11:46:16 +0000

    [update] admin/customer作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,7 +1,25 @@
 class Admin::CustomersController < ApplicationController
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer), notice: '会員情報を更新しました。'
+    else
+      flash.now[:notice] = '必要し事項を入力してください。'
+      render :edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :email, :is_active)
   end
 end
