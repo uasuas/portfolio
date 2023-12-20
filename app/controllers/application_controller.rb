debd9c70f30623a9e168e6df13ec48c283500c04
usausa <143808163+uasuas@users.noreply.github.com>
2023-12-20 04:14:01 +0000

    [update] public/line_plan/indexレイアウト修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     # 管理者のログアウト時。
     if resource.is_a?(Admin)
-      admin_root_path
+      root_path
     # ユーザーのログアウト時。
     elsif resource.is_a?(Customer)
       root_path
