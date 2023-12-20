b2dd80674936c89a310cd50ad594e99b28c0b64f
usausa <143808163+uasuas@users.noreply.github.com>
2023-12-20 01:53:00 +0000

    [update] アクセス制限

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_customer!, except: [:top, :about]
   # deviseのコントローラーが実行されている場合にのみメソッドを呼び出す。
   before_action :configure_permitted_parameters, if: :devise_controller?
   # 管理者としてログインしていない場合アクセス制限をするが、その前にadmin_area?の戻り値を確認してtrueなら実行する。
