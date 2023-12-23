09fc399c2df8d3963e365f228594e5c84f1e9d3e
usausa <143808163+uasuas@users.noreply.github.com>
2023-12-23 03:14:43 +0000

    [update] backtop微修正

--- app/javascript/backtop.js
+++ app/javascript/backtop.js
@@ -1,7 +1,16 @@
 $(function() {
-  $('#back a').on('click',function(event){
+  $('#back').hide();
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 0) {
+      $('#back').fadeIn();
+    } else {
+      $('#back').fadeOut();
+    }
+  });
+
+  $('#back a').on('click', function(event) {
     $('body, html').animate({
-      scrollTop:0
+      scrollTop: 0
     }, 800);
     event.preventDefault();
   });
