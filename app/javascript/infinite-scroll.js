c224fb6a89c3f7030f36f703aca290156828042b
usausa <143808163+uasuas@users.noreply.github.com>
2023-12-24 14:54:46 +0000

    [update]　ページネーション修正

--- app/javascript/infinite-scroll.js
+++ app/javascript/infinite-scroll.js
@@ -1,4 +1,4 @@
-$(function(){
+$(document).on('turbolinks:load', function(){
   if ($(".pagination a[rel=next]").length) {
     $('.infinite').infiniteScroll({
       path: ".pagination a[rel=next]",
